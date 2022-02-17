64e0222393863eade4b65901b0ffcc592009c7d3
Tulio Filho <tuliofilho@outlook.com>
2022-02-17 20:00:12 -0300

    on pronounciation route fix bug

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ const presentPerfectRoutes = require("./api/routes/presentPerfectRoutes");
 app.use("/present-perfect", presentPerfectRoutes);
 
 const onPronounciationRoutes = require("./api/routes/onPronounciationRoutes");
-app.use("/on-perfect", onPronounciationRoutes);
+app.use("/on-pronounciation", onPronounciationRoutes);
 
 const PORT = process.env.PORT || 5050;
 app.listen(PORT, () => console.log(`Server is running in port ${PORT}`));
