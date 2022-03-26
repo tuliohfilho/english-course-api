df6400066e749f4493cbd0732b8c667165eef1f9
Tulio Filho <tuliofilho@outlook.com>
2022-03-26 20:12:20 -0300

    test no dist folder

--- dist/routes/irregular-verbs.routes.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.irregularVerbRoutes = void 0;
-const express_1 = require("express");
-const getAll_1 = require("../modules/irregularVerbs/useCases/getAll");
-const irregularVerbRoutes = (0, express_1.Router)();
-exports.irregularVerbRoutes = irregularVerbRoutes;
-const getAllIrregularVerbsController = new getAll_1.GetAllIrregularVerbsController();
-irregularVerbRoutes.get("/", getAllIrregularVerbsController.handle);
