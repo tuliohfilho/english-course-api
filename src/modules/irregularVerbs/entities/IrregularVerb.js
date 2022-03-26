86421b291d1f9ec6d025e82f105920c2d79df659
Tulio Filho <tuliofilho@outlook.com>
2022-03-26 19:04:54 -0300

    new js files

---
+++ src/modules/irregularVerbs/entities/IrregularVerb.js
@@ -0,0 +1,15 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.IrregularVerb = void 0;
+class IrregularVerb {
+    constructor({ id, past, audioName, infinitve, translation, pastParticiple, }) {
+        this.id = id;
+        this.past = past;
+        this.audioName = audioName;
+        this.infinitve = infinitve;
+        this.translation = translation;
+        this.pastParticiple = pastParticiple;
+    }
+}
+exports.IrregularVerb = IrregularVerb;
+//# sourceMappingURL=IrregularVerb.js.map
---
+++ src/server.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+require("reflect-metadata");
+require("./shared/container");
+const _1 = require(".");
+const PORT = process.env.PORT || 5050;
+_1.app.listen(PORT, () => console.log(`Server is running in port ${PORT}`));
+//# sourceMappingURL=server.js.map
