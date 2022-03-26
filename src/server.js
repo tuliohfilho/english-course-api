fb9bfe41edc51217e269c42c96dcf5984874bc20
Tulio Filho <tuliofilho@outlook.com>
2022-03-26 19:09:18 -0300

    test change serve folder

--- src/server.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-require("reflect-metadata");
-require("./shared/container");
-const _1 = require(".");
-const PORT = process.env.PORT || 5050;
-_1.app.listen(PORT, () => console.log(`Server is running in port ${PORT}`));
-//# sourceMappingURL=server.js.map
