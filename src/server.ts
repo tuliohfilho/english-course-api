7e8827cfafc388cf34e895e7344f40d04a339d14
Tulio Filho <tuliofilho@outlook.com>
2022-03-26 18:23:34 -0300

    teste new config vercel

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,7 @@
 import "reflect-metadata";
-import "./src/shared/container";
+import "./shared/container";
 
-import { app } from "./src";
+import { app } from ".";
 
 const PORT = process.env.PORT || 5050;
 app.listen(PORT, () => console.log(`Server is running in port ${PORT}`));
