d3bb6607ca2074aa21630ff20758d76a5784f37b
Tulio Filho <tuliofilho@outlook.com>
2022-03-26 16:15:37 -0300

    Change build src and routes dest in vecel.json

---
+++ src/server.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+require("reflect-metadata");
+require("./shared/container");
+const _1 = require(".");
+_1.app.listen(3333, () => console.log("Server is running"));
