4c33102ceec0557757493699e324b753fb8b60c3
Tulio Filho <tuliofilho@outlook.com>
2022-03-26 16:22:58 -0300

    add *.js in gitignore

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,21 @@ import { router } from "./routes";
 const app = express();
 app.use(express.json());
 
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
+
 app.use(router);
 
 export { app };
