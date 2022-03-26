282cd94675145b4d52c8866a6b30352d3131a75f
Tulio Filho <tuliofilho@outlook.com>
2022-03-26 19:10:36 -0300

    new serve.js

---
+++ server.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+require("reflect-metadata");
+require("./src/shared/container");
+const src_1 = require("./src");
+const PORT = process.env.PORT || 5050;
+src_1.app.listen(PORT, () => console.log(`Server is running in port ${PORT}`));
+//# sourceMappingURL=server.js.map
