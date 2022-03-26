1d93d9f39e0ca50cad131778dfc025c6ef75f504
Tulio Filho <tuliofilho@outlook.com>
2022-03-26 17:34:41 -0300

    generate js files

---
+++ src/index.js
@@ -0,0 +1,20 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.app = void 0;
+const express_1 = __importDefault(require("express"));
+const routes_1 = require("./routes");
+const app = (0, express_1.default)();
+exports.app = app;
+app.use(express_1.default.json());
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+app.use(express_1.default.urlencoded({
+    extended: true,
+}));
+app.use(routes_1.router);
---
+++ src/routes/irregular-verbs.routes.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.irregularVerbRoutes = void 0;
+const express_1 = require("express");
+const getAll_1 = require("../modules/irregularVerbs/useCases/getAll");
+const irregularVerbRoutes = (0, express_1.Router)();
+exports.irregularVerbRoutes = irregularVerbRoutes;
+const getAllIrregularVerbsController = new getAll_1.GetAllIrregularVerbsController();
+irregularVerbRoutes.get("/", getAllIrregularVerbsController.handle);
